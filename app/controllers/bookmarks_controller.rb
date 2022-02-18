74066d99de85b3c276fa014af784498132dc8862
Sedrak Nadzharyan <sedrak005@gmail.com>
2022-02-18 16:23:12 -0500

    fixed rake

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -26,7 +26,7 @@ class BookmarksController < ApplicationController
     @bookmark.list = @list
 
     if @bookmark.save
-      redirect_to @bookmark, notice: 'bookmark was successfully created.'
+      redirect_to @list, notice: 'bookmark was successfully created.'
     else
       render :new
     end
@@ -55,6 +55,6 @@ class BookmarksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def bookmark_params
-      params.require(:bookmark).permit(:comment, :movie)
+      params.require(:bookmark).permit(:comment, :movie_id)
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,33 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
-puts "starterd"
-List.destroy_all
+# require 'faker'
+# puts "starterd"
+# List.destroy_all
+# Movie.destroy_all
+# Bookmark.destroy_all
+
+# def create_1
+#   begin
+#     List.create!(name: Faker::Book.genre)
+#     Movie.create!(title: Faker::Movie.title, overview: Faker::Restaurant.description, poster_url: 'https://source.unsplash.com/random/900×700/?movie', rating: rand(0.0..1.0))
+#   rescue ActiveRecord::RecordInvalid => invalid
+#     puts invalid.record.errors
+#     # create
+#   end
+# end
+
+# def create_2
+#   begin
+#     Bookmark.create!(comment: Faker::Food.ethnic_category, movie: Movie.all.sample, list: List.all.sample)
+#   rescue ActiveRecord::RecordInvalid => invalid
+#     puts invalid.record.errors
+#     # create
+#   end
+# end
+
+# 20.times{create_1}
+# 40.times{create_2}
+
+
+# puts "done"
+
+require 'open-uri'
+
+puts 'Deleting db....'
 Movie.destroy_all
-Bookmark.destroy_all
 
-def create_1
-  begin
-    List.create!(name: Faker::Book.genre)
-    Movie.create!(title: Faker::Movie.title, overview: Faker::Restaurant.description, poster_url: 'https://source.unsplash.com/random/900×700/?movie', rating: rand(0.0..1.0))
-  rescue ActiveRecord::RecordInvalid => invalid
-    puts invalid.record.errors
-    # create
-  end
+puts 'Creating movies....'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+json = URI.open(url).read
+movies = JSON.parse(json)
+movies['results'].each do |movie|
+  Movie.create!(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
 end
-
-def create_2
-  begin
-    Bookmark.create!(comment: Faker::Food.ethnic_category, movie: Movie.all.sample, list: List.all.sample)
-  rescue ActiveRecord::RecordInvalid => invalid
-    puts invalid.record.errors
-    # create
-  end
-end
-
-20.times{create_1}
-40.times{create_2}
-
-
-puts "done"
