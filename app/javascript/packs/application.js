4aa0139ccdec8343da134561c9d23ad4aff91c73
Sedrak Nadzharyan <sedrak005@gmail.com>
2022-02-18 11:08:27 -0500

    done with index and show

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,18 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require turbolinks
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require fontawesome/all
+//= require_tree .
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+require("@fortawesome/fontawesome-free");
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+puts "starterd"
+def create
+  l = List.create!(name: Faker::Book.genre)
+  m = Movie.create!(title: Faker::Movie.title, overview: Faker::Restaurant.description, poster_url: 'https://source.unsplash.com/random/900×700/?movie', rating: rand(0.0..1.0))
+  Bookmark.create!(comment: Faker::Food.ethnic_category, movie: m, list: l)
+
+end
+
+14.times do
+  # begin
+
+  # rescue TypeError
+  # end
+
+  begin
+    create
+  rescue ActiveRecord::RecordInvalid => invalid
+    puts invalid.record.errors
+  end
+end
+
+
+puts "done"
